6838ea8385fcb5f17eddd812ad32d07e406ef681
Mateus Auler <mateus.auler@sga.pucminas.br>
2018-10-28 18:31:10 -0300

    Fixed quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -1,38 +1,47 @@
+void quicksortR(int* arr, int start, int end);
+void swap(int* arr, int i, int j);
+
+// Quicksort entry-point
+void quicksort(int* arr, int len)
+{
+    if (len > 0)
+        quicksortR(arr, 0, len - 1);
+}
+
+// Recursive quicksort implementarion
+void quicksortR(int* arr, int start, int end)
+{
+    int i = start;
+    int j = end;
+    int pivot = arr[(i + j) / 2]; // Get the element in the middle of the array and set it as the pivot
+
+    if (start < end)
+    {
+        while(i <= j)
+        {
+            // Increment i until the value it's pointed at is greater than the pivot
+            while (arr[i] < pivot) i++;
+
+            // Decrement j until the value it's pointed at is lesser than the pivot
+            while (arr[j] > pivot) j--;
+
+            if (i <= j)
+            {
+                swap(arr, i, j);
+                i++;
+                j--;
+            }
+        }
+
+        if (start < j) quicksortR(arr, start, j); // Calls quicksort on the left-most part of the array
+        if (end   > i) quicksortR(arr, i,   end); // Calls quicksort on the right-most part of the array
+    }
+}
+
 // Swaps two values in an integer array
 void swap(int* arr, int i, int j)
 {
     int tmp = arr[i];
     arr[i] = arr[j];
     arr[j] = tmp;
-}
-
-// Recursive quicksort implementarion
-void quicksort(int* arr, int start, int end)
-{
-    int i = start;
-    int j = end;
-    int pivot = arr[(j + i) / 2]; // Get the element in the middle of the array and set it as the pivot
-
-    while(i < j)
-    {
-        // Increment i until the value it's pointed at is greater than the pivot
-        while (arr[i] < pivot) i++;
-
-        // Decrement j until the value it's pointed at is lesser than the pivot
-        while (arr[j] > pivot) j--;
-
-        swap(arr, i, j);
-        i++;
-        j--;
-    }
-
-    if (start < j) quicksort(arr, start, j - 1); // Calls quicksort on the left-most half of the array
-    if (end   > i) quicksort(arr, i + 1,   end); // Calls quicksort on the right-most half of the array
-}
-
-// Quicksort entry-point
-void quicksort(int* arr, int len)
-{
-    if (len > 0)
-        quicksort(arr, 0, len);
 }
