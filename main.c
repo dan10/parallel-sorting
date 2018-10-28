6b1801039d974300f821302df554396c3813bc15
Mateus Auler <mateus.auler@sga.pucminas.br>
2018-10-28 18:30:15 -0300

    Added printArr() and funcion declarations to main

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+void printArr(int* arr, int len);
+void quicksort(int* arr, int len);
+
 void main()
 {
+
+}
+
+// Prints and integer array 'arr' of length 'len'
+void printArr(int* arr, int len)
+{
+    int i = 0;
+
+    printf("[ ");
+
+    for(i = 0; i < len; i++) printf("%d ", arr[i]);
     
+    printf("]\n");
 }
